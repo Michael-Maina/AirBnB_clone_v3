72e3f8937001d9e3d13ebe1d8373c5863e5febc7
Michael-Maina <michaelmaina02@gmail.com>
2023-05-02 00:34:19 +0300

    Added task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,13 @@ Initializes an instance of the Flask web framework
 """
 from api.v1.views import app_views
 from flask import Blueprint, Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, origins='0.0.0.0')
 
 @app.teardown_appcontext
 def teardown(td):
