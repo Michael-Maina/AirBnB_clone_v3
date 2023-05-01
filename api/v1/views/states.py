e802e65cae36dd418733ecdc752dc1391f9b36a8
Michael-Maina <michaelmaina02@gmail.com>
2023-05-02 00:24:06 +0300

    Addressing bugs

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,7 +22,7 @@ def r_state_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id.', methods=['DELETE'],
+@app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_state(state_id):
     """Deletes a State object"""
